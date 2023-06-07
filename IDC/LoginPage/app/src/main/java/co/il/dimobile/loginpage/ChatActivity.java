f41833bb565b86c2355e7970b83f0b4a33a22fa4
odeliahkanzen <odeliah@odeliah.com>
2023-06-07 20:13:52 +0300

    v2

---
+++ IDC/LoginPage/app/src/main/java/co/il/dimobile/loginpage/ChatActivity.java
@@ -0,0 +1,118 @@
+package co.il.dimobile.loginpage;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.bumptech.glide.Glide;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ChatActivity extends AppCompatActivity {
+
+    GoogleSignInAccount account;
+    MessageAdapter adapter;
+
+    RequestQueue mRequestQue;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+
+        mRequestQue = Volley.newRequestQueue(this);
+
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            account = (GoogleSignInAccount) extras.get("user");
+        }
+
+        TextView welcome = findViewById(R.id.welcomeTV);
+        welcome.setText("Welcome, " + account.getDisplayName());
+
+        ImageView userImage = findViewById(R.id.mainUserImage);
+        Glide.with(this).load(account.getPhotoUrl()).into(userImage);
+
+        adapter = new MessageAdapter(account.getId());
+        RecyclerView recycler = findViewById(R.id.chatRV);
+        recycler.setHasFixedSize(false);
+        RecyclerView.LayoutManager manager = new GridLayoutManager(getApplicationContext(), 1);
+        recycler.setLayoutManager(manager);
+
+        adapter.registerAdapterDataObserver(new RecyclerView.AdapterDataObserver() {
+            @Override
+            public void onChanged() {
+                recycler.smoothScrollToPosition(adapter.getItemCount() - 1);
+            }
+        });
+
+        recycler.addOnLayoutChangeListener((view, i, i1, i2, i3, i4, i5, i6, i7) -> {
+            if (adapter.getItemCount() > 0) {
+                recycler.smoothScrollToPosition(adapter.getItemCount() - 1);
+            }
+        });
+
+        recycler.setAdapter(adapter);
+
+        FloatingActionButton btn = findViewById(R.id.addMessageBtn);
+        btn.setOnClickListener(view -> {
+            EditText text = findViewById(R.id.messageET);
+            String messageText = text.getText().toString();
+            chatMessage m = new chatMessage(account.getPhotoUrl().toString(), account.getDisplayName(), account.getId(), messageText);
+            adapter.addMessage(m);
+            text.setText("");
+            sendCloudMessage(messageText);
+        });
+    }
+
+    private void sendCloudMessage(String message) {
+        String serverKey = "AAAAVKKMYTk:APA91bGY5kzO-5NYqRCU-DAu2rJdF1fLnLImjbDLj8JCk8pBGN2ahHIcv8hAX2woGmCdFfdaha0m5NsQpZmExZUZK15W1aahu2Orv_9TTibBYXDQn03bC76Rivuiq9qdOfdqC6Z2uYrO";
+        String topic = "chat";
+
+        JSONObject json = new JSONObject();
+        try {
+            json.put("to", "/topics/" + topic);
+            JSONObject notificationObj = new JSONObject();
+            notificationObj.put("title", account.getDisplayName());
+            notificationObj.put("body", message);
+            json.put("notification", notificationObj);
+
+            String URL = "https://fcm.googleapis.com/fcm/send";
+
+            JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, URL,
+                    json,
+                    response -> Log.d("MUR", "onResponse: " + response.toString()),
+                    error -> Log.d("MUR", "onError: " + error.networkResponse)
+            ) {
+                @Override
+                public Map<String, String> getHeaders() {
+                    Map<String, String> header = new HashMap<>();
+                    header.put("content-type", "application/json");
+                    header.put("authorization", "key=" + serverKey);
+                    return header;
+                }
+            };
+
+            mRequestQue.add(request);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
--- IDC/LoginPage/app/src/main/java/co/il/dimobile/loginpage/MainActivity.java
+++ IDC/LoginPage/app/src/main/java/co/il/dimobile/loginpage/MainActivity.java
@@ -1,13 +1,23 @@
 package co.il.dimobile.loginpage;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.analytics.FirebaseAnalytics;
+import android.widget.Button;
+import com.google.android.gms.common.api.ApiException;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int RC_SIGN_IN = 123;
+    private GoogleSignInClient mGoogleSignInClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,10 +25,44 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         FirebaseAnalytics mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        Button signIn = findViewById(R.id.login_button);
+        signIn.setOnClickListener(view -> {
+            Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+            startActivityForResult(signInIntent, RC_SIGN_IN);
+        });
     }
 
-    GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            .requestEmail()
-            .build();
-    mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+            //Toast.makeText(this,"Welcome " + account.getDisplayName(),Toast.LENGTH_LONG).show();
+
+            // Signed in successfully, show authenticated UI.
+            Intent i = new Intent(MainActivity.this,ChatActivity.class);
+            i.putExtra("user",account);
+            startActivity(i);
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+        }
+    }
 }
